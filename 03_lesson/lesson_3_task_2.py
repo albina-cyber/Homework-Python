863aad454e3287852debfabc32460af5f2f6914f
albina-cyber <palalbina2008/nk@gmail.com>
2025-06-13 16:57:23 +0300

    lesson_3_task_2

--- 03_lesson/lesson_3_task_2.py
+++ 03_lesson/lesson_3_task_2.py
@@ -2,10 +2,10 @@ from smartphone import Smartphone
 
 catalog = [
     Smartphone(brand="Apple", model="14", number="+79270448857"),
-    Smartphone(brand="Lenovo", model="12", number= "+79064587952"),
+    Smartphone(brand="Lenovo", model="12", number="+79064587952"),
     Smartphone(brand="Honor", model="30i", number="+79050000000"),
     Smartphone(brand="Samsung", model="M31S", number="+79390362555"),
-    Smartphone(brand ="Techno", model="524", number="+79112224433")
+    Smartphone(brand="Techno", model="524", number="+79112224433")
 ]
 for smartphone in catalog:
     print(f"{smartphone.brand} - {smartphone.model}. {smartphone.number}.")
