945daeecf9dbfaa16b901455e17dcd63bc10db74
albina-cyber <palalbina2008/nk@gmail.com>
2025-06-13 13:07:46 +0300

    adress

--- 03_lesson/adress.py
+++ 03_lesson/adress.py
@@ -1,9 +1,9 @@
 class Address:
     def __init__(self, index, city, street, home_number, apart_number):
-        self.c = city
-        self.s = street
-        self.h = home_number
-        self.an = apart_number
+        self.city = city
+        self.street = street
+        self.home = home_number
+        self.apart = apart_number
 
     def __str__(self):
-        return f"{self.c} {self.s}{self.h} {self.an}"
+        return f"{self.city} {self.street}{self.home} {self.apart}"
