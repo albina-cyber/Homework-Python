f89a4fc7808cc17f351bdc0cbbfec6c1d50c0dd8
albina-cyber <palalbina2008/nk@gmail.com>
2025-07-23 20:20:56 +0300

    07_lesson

--- 06K_lesson/test_02_calc.py
+++ 06K_lesson/test_02_calc.py
@@ -7,20 +7,18 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 
 class Calculator:
-
     def __init__(self, driver):
         self._driver = driver
-        self._driver.get(
-            "https://bonigarcia.dev/selenium-webdriver-java/"
-            "slow-calculator.html"
-            )
+        self._driver.get
+        ("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
 
     def delay(self, time: str):
         self._driver.find_element(By.CSS_SELECTOR, "#delay").clear()
         self._driver.find_element(By.CSS_SELECTOR, "#delay").send_keys(time)
 
     def button(self):
-        self._driver.find_element(
+        self._driver.find_element
+        (
             By.CSS_SELECTOR, "#calculator > div.keys > span:nth-child(1)"
         ).click()
 
--- Lesson7/test_labirint.py
+++ Lesson7/test_labirint.py
@@ -1,26 +1,31 @@
-
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
-
-driver = webdriver.Chrome(service=ChromeService
-                          (ChromeDriverManager().install()))
-driver.maximize_window
-
-
 cookie = {"name": "cookie_policy", "value": "1"}
 
 
 def test_card_counter():
-    browser = webdriver.Chrome()
+    browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().
+                                                     install()))
+    browser.maximize_window
+
+# Перейти на сайт: labirint.ru
+
+    browser.get("https://www.labirint.ru/")
+    browser.implicitly_wait(4)
+    browser.add_cookie(cookie)
+
+# Найти все книги по слову Python
+
+    browser.find_element(By.CSS_SELECTOR, "#search-field").send_keys('python')
+
+    sleep(5)
+
+    browser.quit()
 
 
-browser.get("https://www.labirint.ru")
-browser.implicitly_wait(4)
-browser.maximize_window()
-browser.add_cookie(cookie)
-
-
-sleep(5)
+# Добавить все книги в корзину.
+# Перейти в корзину.
+# Проверить, что счетчик товаров соответствует количеству добавленных книг.
